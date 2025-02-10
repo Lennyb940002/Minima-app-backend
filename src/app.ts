60ace2ce02ff3130d1ccb4f3dfd57e7db02bf4d2
Lennyb940002 <146711861+Lennyb940002@users.noreply.github.com>
2025-02-10 19:08:39 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,11 @@ const app = express();
 // Connexion à MongoDB
 connectDB();
 
-// Configuration de CORS
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
+  origin: [
+    'https://minima-app-frontend.vercel.app',
+    'http://localhost:3000'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
