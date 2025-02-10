2590e6ff7aa02c58207a7c888a1b2290af3a0599
Lennyb940002 <146711861+Lennyb940002@users.noreply.github.com>
2025-02-10 15:13:15 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -18,9 +18,7 @@ connectDB();
 
 // Configure CORS options
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'production'
-    ? ['https://minima-app-frontend.vercel.app']
-    : 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
